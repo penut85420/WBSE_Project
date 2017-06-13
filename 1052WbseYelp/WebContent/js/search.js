eb3abf1d362ee70b6eaa380fabbf66327de1eae3
hsnu131206 <hsnu131206@gamil.com>
2017-06-13 12:54:46 +0800

    單獨拉出serch功能

---
+++ 1052WbseYelp/WebContent/js/search.js
@@ -0,0 +1,46 @@
+		/*搜尋列表*/
+		modalGenerator("搜尋結果列表",
+				"<div class='row'>"+
+			    "	<div class='col-md-12'>"+
+	       		"		<div class='list-group'>"+
+				"			<a href='searchUI.jsp' class='list-group-item'>"+
+				"				<div class='pull-left'>"+
+				"					<img class='img-thumbnail' src='picture/logo.png' style='float:left;height:70px;width:70px;'>"+
+				"				</div>"+
+				"				<div class='pull-left' style='margin-left:1vw;text-overflow:ellipsis;'>"+
+				"					<h4 class='list-group-item-heading'>Name : 星巴克-信義區-4</h4>"+
+				"					<p class='list-group-item-text'>Distance : 4400KM</p>"+
+				"					<p class='list-group-item-text'>Rating : ★★★★☆</p>"+
+				"					<p class='list-group-item-text'>PhoneNumber : 02 25578493</p>"+
+				"				</div>"+
+				"				<div class='pull-right' style='margin-left:1vw;text-overflow:ellipsis;'>"+
+				"					<h4 class='list-group-item-heading'>信義</h4>"+
+				"					<p class='list-group-item-text'>11 Bao'an Street</p>"+
+				"					<p class='list-group-item-text'>保安街11號</p>"+
+				"					<p class='list-group-item-text'>Datong District, 台北市 103</p>"+
+				"					<p class='list-group-item-text'>Taiwan</p>"+
+				"				</div>"+
+				"				<div class='clearfix'></div>"+
+				"			</a>"+
+				"			<a href='searchUI.jsp' class='list-group-item'>"+
+				"				<div class='pull-left'>"+
+				"					<img class='img-thumbnail' src='picture/logo.png' style='float:left;height:70px;width:70px;'>"+
+				"				</div>"+
+				"				<div class='pull-left' style='margin-left:1vw;text-overflow:ellipsis;'>"+
+				"					<h4 class='list-group-item-heading'>Name : 星巴克-信義區-4</h4>"+
+				"					<p class='list-group-item-text'>Distance : 4400KM</p>"+
+				"					<p class='list-group-item-text'>Rating : ★★★★☆</p>"+
+				"					<p class='list-group-item-text'>PhoneNumber : 02 25578493</p>"+
+				"				</div>"+
+				"				<div class='pull-right' style='margin-left:1vw;text-overflow:ellipsis;'>"+
+				"					<h4 class='list-group-item-heading'>信義</h4>"+
+				"					<p class='list-group-item-text'>11 Bao'an Street</p>"+
+				"					<p class='list-group-item-text'>保安街11號</p>"+
+				"					<p class='list-group-item-text'>Datong District, 台北市 103</p>"+
+				"					<p class='list-group-item-text'>Taiwan</p>"+
+				"				</div>"+
+				"				<div class='clearfix'></div>"+
+				"			</a>"+
+				"		</div>"+
+       			"	</div>"+
+			    "</div>");
